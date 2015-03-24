6a189abe8d8827e8c92431ae9c1ce7267daf2a16
Paul Daigle <paul@bitpay.com>
2015-03-23 20:09:15 -0400

    Basic cli works
    Saves the preferences with "new" command
    Reads the preferences and saves a token with "pair" command
    Reads the prefs and the token to create invoices with "createinvoice"
    Reads prefs to get invoice with "getinvoice" command

---
+++ src/gobitpay/client_methods.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	bp "github.com/philosodad/bitpay-go/client"
+	ku "github.com/philosodad/bitpay-go/key_utils"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+func getInvoice(invId string) (price float64, currency string, err error) {
+	rootPath := os.ExpandEnv("$HOME")
+	client := clientFromFiles(rootPath)
+	inv, err := client.GetInvoice(invId)
+	if err == nil {
+		price = inv.Price
+		currency = inv.Currency
+		return price, currency, err
+	}
+	return price, currency, err
+}
+
+func createInvoice(price string, currency string) (invId string, err error) {
+	rootPath := os.ExpandEnv("$HOME")
+	client := clientFromFiles(rootPath)
+	parsedPrice, _ := strconv.ParseFloat(price, 64)
+	tok, _ := ioutil.ReadFile(rootPath + "/.bp/token.json")
+	var token bp.Token
+	json.Unmarshal(tok, &token)
+	client.Token = token
+	inv, err := client.CreateInvoice(parsedPrice, currency)
+	invId = inv.Id
+	return invId, err
+}
+
+func pairClient(code string) (token string, err error) {
+	rootPath := os.ExpandEnv("$HOME")
+	client := clientFromFiles(rootPath)
+	var toke bp.Token
+	toke, err = client.PairWithCode(code)
+	var tokenByte []byte
+	tokenByte, err = json.Marshal(toke)
+	ioutil.WriteFile(rootPath+"/.bp/token.json", tokenByte, 0644)
+	token = string(tokenByte)
+	return token, err
+}
+
+func clientFromFiles(rootPath string) bp.Client {
+	pm, _ := ioutil.ReadFile(rootPath + "/.bp/bitpay.pem")
+	uri, _ := ioutil.ReadFile(rootPath + "/.bp/uri.txt")
+	notsecure, _ := ioutil.ReadFile(rootPath + "/.bp/insecure.txt")
+	var insecure bool
+	if string(notsecure) == "true" {
+		insecure = true
+	} else {
+		insecure = false
+	}
+	client := bp.Client{ApiUri: string(uri), Insecure: insecure, Pem: string(pm)}
+	return client
+}
+
+func newClient(uri string, insecure bool) {
+	pm := ku.GeneratePem()
+	notSecure := "false"
+	if insecure {
+		notSecure = "true"
+	}
+	rootPath := os.ExpandEnv("$HOME")
+	os.Mkdir(rootPath+"/.bp", 0755)
+	pmByte := []byte(pm)
+	uriByte := []byte(uri)
+	secByte := []byte(notSecure)
+	ioutil.WriteFile(rootPath+"/.bp/bitpay.pem", pmByte, 0644)
+	ioutil.WriteFile(rootPath+"/.bp/uri.txt", uriByte, 0644)
+	ioutil.WriteFile(rootPath+"/.bp/insecure.txt", secByte, 0644)
+}
--- src/gobitpay/gobitpay.go
+++ src/gobitpay/gobitpay.go
@@ -1,16 +1,117 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codegangsta/cli"
 	"os"
 )
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "boom"
-	app.Usage = "make an explosive entrance"
-	app.Action = func(c *cli.Context) {
-		println("boom! I say!")
+	app.Name = "gobitpay"
+	app.Usage = "gobitpay <command> [opts]"
+	app.Commands = []cli.Command{
+		{
+			Name:  "new",
+			Usage: "bitpaygo new --[test|staging|custom <url> [--insecure]]",
+			Subcommands: []cli.Command{
+				{
+					Name:    "live",
+					Aliases: []string{"l"},
+					Usage:   "sets up a live, aye",
+					Action: func(c *cli.Context) {
+						newClient("https://bitpay.com", false)
+					},
+				},
+				{
+					Name:  "test",
+					Usage: "sets up a test, aye",
+					Action: func(c *cli.Context) {
+						newClient("https://test.bitpay.com", false)
+					},
+				},
+				{
+					Name:  "staging",
+					Usage: "sets up a staging server, aye",
+					Action: func(c *cli.Context) {
+						newClient("https://staging.b-pay.net", false)
+					},
+				},
+				{
+					Name:  "custom",
+					Usage: "sets up a custom server: ",
+					Flags: []cli.Flag{
+						cli.BoolFlag{
+							Name:  "insecure",
+							Usage: "new client will ignore ssl errors",
+						},
+					},
+					Action: func(c *cli.Context) {
+						apiuri := ""
+						var insecure bool
+						insecure = c.Bool("insecure")
+						if len(c.Args()) > 0 {
+							apiuri = c.Args()[0]
+							newClient(apiuri, insecure)
+
+						} else {
+							println("custom server requires a server address")
+						}
+					},
+				},
+			},
+		},
+		{
+			Name:  "pair",
+			Usage: "bitpaygo pair",
+			Action: func(c *cli.Context) {
+				if len(c.Args()) > 0 {
+					code := c.Args()[0]
+					_, err := pairClient(code)
+					if err != nil {
+						fmt.Println(err)
+					}
+				} else {
+					println("Must provide a pairing code")
+				}
+			},
+		},
+		{
+			Name:  "createinvoice",
+			Usage: "bitpaygo createinvoice price currency <parameters>",
+			Action: func(c *cli.Context) {
+				if len(c.Args()) > 1 {
+					price := c.Args()[0]
+					currency := c.Args()[1]
+					invId, err := createInvoice(price, currency)
+					if err != nil {
+						fmt.Println(err)
+					} else {
+						println(invId)
+					}
+				} else {
+					println("Must provide a price and a currency")
+				}
+			},
+		},
+		{
+			Name:  "getinvoice",
+			Usage: "bitpaygo getinvoice <invoiceid>",
+			Action: func(c *cli.Context) {
+				if len(c.Args()) > 0 {
+					invId := c.Args()[0]
+					price, currency, err := getInvoice(invId)
+					if err != nil {
+						fmt.Println(err)
+					} else {
+						priceSt := fmt.Sprintf("%f", price)
+						fmt.Println("Price: " + priceSt + ", Currency: " + currency)
+					}
+				} else {
+					println("Must provide an invoice id")
+				}
+			},
+		},
 	}
 
 	app.Run(os.Args)
