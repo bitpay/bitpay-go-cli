e4cb775b7ed1dd3673f710abd3d8b8b75d6726fe
Paul Daigle <paul@bitpay.com>
2015-04-27 14:33:17 -0400

    Add completed pair_steps.py file

--- helpers/pair_steps.py
+++ helpers/pair_steps.py
@@ -10,15 +10,42 @@ USER_NAME = os.environ['RCTESTUSER']
 PASSWORD = os.environ['RCTESTPASSWORD']
 
 def get_claim_code_from_server():
-  browser = Browser('phantomjs', service_args=['--ignore-ssl-errors=true'])
-  browser.visit(ROOT_ADDRESS + "/merchant-login")
-  browser.fill_form({"email": USER_NAME, "password": PASSWORD})
-  browser.find_by_id("loginButton")[0].click()
-  time.sleep(1)
-  browser.visit(ROOT_ADDRESS + "/api-tokens")
-  browser.find_by_css(".token-access-new-button").find_by_css(".btn").find_by_css(".icon-plus")[0].click()
-  browser.find_by_id("token-new-form").find_by_css(".btn")[0].click()
-  return browser.find_by_css(".token-claimcode")[0].html
+    time.sleep(5)
+    browser = Browser('phantomjs', service_args=['--ignore-ssl-errors=true'])
+    browser.visit(ROOT_ADDRESS + "/merchant-login")
+    browser.fill_form({"email": USER_NAME, "password": PASSWORD})
+    browser.find_by_id("loginButton")[0].click()
+    time.sleep(1)
+    browser.visit(ROOT_ADDRESS + "/api-tokens")
+    code = get_code_from_page(browser, "thiscodewillneverbevalid")
+    gopath = os.environ['GOPATH']
+    tempath = gopath + "/temp"
+    if not os.path.exists(tempath):
+        os.makedirs(tempath)
+    write_code_to_file(code, tempath + "/retrievecode.txt")
+    print(code)
+    time.sleep(10)
+    browser.reload()
+    code = get_code_from_page(browser, code)
+    write_code_to_file(code, tempath + "/paircode.txt")
+    print(code)
+    time.sleep(10)
+    browser.reload()
+    code = get_code_from_page(browser, code)
+    write_code_to_file(code, tempath + "/invoicecode.txt")
+    print(code)
+    return code
 
-code = get_claim_code_from_server()
-sys.stdout.write(code)
+def get_code_from_page(browser, code):
+    browser.find_by_css(".token-access-new-button").find_by_css(".btn").find_by_css(".icon-plus")[0].click()
+    browser.find_by_id("token-new-form").find_by_css(".btn")[0].click()
+    newcode = browser.find_by_css(".token-claimcode")[0].html
+    return newcode
+
+def write_code_to_file(code, fname):
+    f = open(fname, 'w')
+    f.write(code)
+    f.close()
+
+get_claim_code_from_server()
+print("done")
--- helpers/run_tests.sh
+++ helpers/run_tests.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-pair_steps.py
+python helpers/pair_steps.py
 ginkgo -r --keepGoing src/github.com/bitpay/bitpay-go
